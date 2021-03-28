1dbac1d3a4e157b0d9288b7713f59d757f4183a0
roamiiing <unf.hwm@ya.ru>
2021-03-28 19:22:48 +0700

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const yup = require('yup');
+const cors = require('cors');
 
 const schema = yup.object({
   name: yup
@@ -39,6 +40,7 @@ const transporter = nodemailer.createTransport({
 const app = express();
 
 app.use(express.json());
+app.use(cors('*'));
 
 app.get('/', (req, res) => {
   res.json({
